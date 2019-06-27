228b6a73a96d7e495c75bbeff809e9841876b574
Tait Hoyem <44244401+TTWNO@users.noreply.github.com>
2019-06-27 15:18:03 +0000

    Add -l option to dmenu

--- config.h
+++ config.h
@@ -7,6 +7,7 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Source Code Pro:size=24" };
 static const char dmenufont[]       = "Source Code Pro:size=24";
+static const char dmenu_list_size[] = "25";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -56,7 +57,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, "-l", dmenu_list_size, NULL};
 static const char *termcmd[]  = { "st", NULL };
 
 static Key keys[] = {
